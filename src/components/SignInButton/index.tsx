7b0b0ff1df80ac0fc3e250f653b8fc2496945254
LucasHenriqueT <lucasthiele0@outlook.com>
2021-10-01 12:40:52 -0300

    sign in button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton(){
+  const isUserLoggedIn = true;
+
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361"/>
+      Lucas Henrique Thiele
+      <FiX color="#737380" className={styles.signOut}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417"/>
+      Sign in with GitHub
+    </button>
+  )
+}
